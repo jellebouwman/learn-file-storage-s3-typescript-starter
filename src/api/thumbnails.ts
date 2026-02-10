1889e359e5c655202690b4dfae3699bbb8526121
Jelle Bouwman <32744870+jellebouwman@users.noreply.github.com>
2026-02-10 14:51:27 +0100

    feat: ch1:l6

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -3,38 +3,7 @@ import { respondWithJSON } from "./json";
 import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
-
-type Thumbnail = {
-  data: ArrayBuffer;
-  mediaType: string;
-};
-
-const videoThumbnails: Map<string, Thumbnail> = new Map();
-
-export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
-  const { videoId } = req.params as { videoId?: string };
-  if (!videoId) {
-    throw new BadRequestError("Invalid video ID");
-  }
-
-  const video = getVideo(cfg.db, videoId);
-  if (!video) {
-    throw new NotFoundError("Couldn't find video");
-  }
-
-  const thumbnail = videoThumbnails.get(videoId);
-  if (!thumbnail) {
-    throw new NotFoundError("Thumbnail not found");
-  }
-
-  return new Response(thumbnail.data, {
-    headers: {
-      "Content-Type": thumbnail.mediaType,
-      "Cache-Control": "no-store",
-    },
-  });
-}
+import { BadRequestError, UserForbiddenError } from "./errors";
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
@@ -60,17 +29,17 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
 
   const mediaType = thumbnail.type;
 
-  const imageBuffer = await thumbnail.arrayBuffer();
+  const imageArrayBuffer = await thumbnail.arrayBuffer();
+  const imageBuffer = Buffer.from(imageArrayBuffer);
+  const imageString = imageBuffer.toString("base64");
   const videoMetaData = getVideo(cfg.db, videoId);
+  const dataURL = `data:${mediaType};base64,${imageString}`;
 
   if (videoMetaData?.userID !== userID) {
     throw new UserForbiddenError("Video does not belong to this user");
   }
 
-  videoThumbnails.set(videoId, { data: imageBuffer, mediaType });
-
-  const thumbnailURL = `http://localhost:${cfg.port}/api/thumbnails/${videoMetaData.id}`;
-  const newVideo = { ...videoMetaData, thumbnailURL };
+  const newVideo = { ...videoMetaData, thumbnailURL: dataURL };
 
   updateVideo(cfg.db, newVideo);
 
